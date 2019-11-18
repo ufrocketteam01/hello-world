ed5d336e023ab434964f72be859fbdad5518de39
Gianne <gianneflores18@gmail.com>
2019-11-18 16:24:08 -0500

    Blueprint works

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 jQuery(document).ready(function(){
     "use strict"
-
- 
-
-
-
-   
     $("a.smooth-scroll").click(function (event) {
 
-      event.preventDefault();
-      
-      var section = $(this).attr("href");
+		event.preventDefault();
 
-      $('html, body').animate({
-          scrollTop: $(section).offset().top - -2
-      }, 1250, "easeInOutExpo");
-  });
-   new WOW().init();
+		var section = $(this).attr("href");
 
+		$('html, body').animate({
+		  scrollTop: $(section).offset().top - -2
+		}, 1250, "easeInOutExpo");
+  	});
+   	new WOW().init();
 });
+
+
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+});
+
+$('#element').tooltip('update')
+
+$(document).mousemove( function(e) {    
+    var mouseX = e.pageX - $('#blueprint-rocket').offset().left + 275;
+    var mouseY = e.pageY - $('#blueprint-rocket').offset().top - 565;
+    $('.tooltip').css({'top':mouseY,'left':mouseX}).fadeIn('slow');
+}); 
